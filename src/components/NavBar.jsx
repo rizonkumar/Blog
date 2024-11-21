a783d33fbd4b409a6c36e70a17360a5f4fe4811b
Rizon Rahi <rizon.kumar.rahi@gmail.com>
2024-11-21 13:55:19 +0530

    Feat: Added Desktop Menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Logo from "./Logo";
-import { Menu, X } from "lucide-react";
+import { LogIn, Menu, X } from "lucide-react";
 
 const NavBar = () => {
   const [open, setOpen] = useState(false);
@@ -14,15 +14,32 @@ const NavBar = () => {
       </div>
       {/* Mobile Menu */}
       <div className="md:hidden">
+        {/* MOBILE MENU ICON */}
         <div
           className="cursor-pointer"
           onClick={() => setOpen((prev) => !prev)}
         >
           {open ? <X className="size-6" /> : <Menu className="size-6" />}
         </div>
+        {/* MOBILE LINK LIST */}
+        <div
+          className={`bg-[#e6e6ff]] absolute top-16 flex h-screen w-full flex-col items-center justify-center transition-all ease-in-out ${open ? "-right-0" : "-right-[100%]"}`}
+        >
+          Menu
+        </div>
       </div>
       {/* Desktop Menu */}
-      <div className="hidden md:flex">D</div>
+      <div className="hidden items-center gap-8 font-medium md:flex xl:gap-12">
+        <a href="">Home</a>
+        <a href="">Trending</a>
+        <a href="">Most Popular</a>
+        <a href="">About</a>
+        <a href="">
+          <button className="flex items-center gap-1">
+            Login <LogIn className="size-5 cursor-pointer" />
+          </button>
+        </a>
+      </div>
     </div>
   );
 };
