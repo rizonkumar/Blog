a0e96993a39446812302bbc6fd935b5f0446c3f6
Rizon Rahi <rizon.kumar.rahi@gmail.com>
2024-11-21 13:16:41 +0530

    Feat: Added Menu Icons and Cross

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import { useState } from "react";
 import Logo from "./Logo";
+import { Menu, X } from "lucide-react";
 
 const NavBar = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <div className="flex h-16 w-full items-center justify-between md:h-20">
       {/* LOGO */}
@@ -10,7 +13,14 @@ const NavBar = () => {
         <span className="text-2xl font-bold text-blue-500">Blog</span>
       </div>
       {/* Mobile Menu */}
-      <div className="md:hidden">M</div>
+      <div className="md:hidden">
+        <div
+          className="cursor-pointer"
+          onClick={() => setOpen((prev) => !prev)}
+        >
+          {open ? <X className="size-6" /> : <Menu className="size-6" />}
+        </div>
+      </div>
       {/* Desktop Menu */}
       <div className="hidden md:flex">D</div>
     </div>
