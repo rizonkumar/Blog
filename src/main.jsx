52db203ad7c8b3b013a9b8290fbf85cbe044db7a
Rizon Rahi <rizon.kumar.rahi@gmail.com>
2024-11-25 21:51:01 +0530

    Feat: Added Routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,47 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Homepage from "./routes/Homepage.jsx";
+import Loginpage from "./routes/Loginpage.jsx";
+import PostListPage from "./routes/PostListPage.jsx";
+import SinglePostPage from "./routes/SinglePostPage.jsx";
+import WritePage from "./routes/WritePage.jsx";
+import RegisterPage from "./routes/RegisterPage.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+  },
+  {
+    path: "/login",
+    element: <Loginpage />,
+  },
+  {
+    path: "/posts",
+    element: <PostListPage />,
+  },
+  {
+    path: "/:slug",
+    element: <SinglePostPage />,
+  },
+  {
+    path: "/write",
+    element: <WritePage />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ToastContainer position="bottom-right" />
+      </QueryClientProvider>
+    </ClerkProvider>
+  </StrictMode>,
+);
